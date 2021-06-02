bcc7c8746b387a329f6c326dbc354e4823f83e57
DocErnstz <ernesto800@outlook.com>
2021-06-01 22:01:36 -0300

    login function with sign jwt

--- src/resolvers/UserResolver.ts
+++ src/resolvers/UserResolver.ts
@@ -1,12 +1,39 @@
-import { Resolver, Query, Mutation, Arg} from "type-graphql";
-import { hash } from "bcryptjs";
+import { Resolver, Query, Mutation, Arg, ObjectType, Field} from "type-graphql";
+import { hash, compare } from "bcryptjs";
 import { User } from "../entity/User";
+import {sign} from "jsonwebtoken";
+
+@ObjectType()
+class LoginResponse {
+  @Field()
+  accessToken: String
+}
 @Resolver()
 export class UserResolver  {
   @Query(() => String)
   hello() {
     return "hi!";
   }
+  @Query(() => [User])
+  users() {
+    return User.find();
+  }
+
+  @Mutation(() => LoginResponse)
+  async login(@Arg("email") email: string, @Arg("password") password: string) : Promise<LoginResponse> {
+    const user = await User.findOne({where: { email }});
+    if(!user) {
+      throw new Error("could not find user");
+    }
+    const valid = compare(password, user.password);
+    if(!valid) {
+      throw new Error("bad password");
+    }
+    return {
+      accessToken: sign({ userId: user.id }, "asd", { expiresIn: "15d" })
+    }
+  }
+  
   @Mutation(() => Boolean)
   async register(
     @Arg("email") email: string,
