abd101d8eea4e1c416b63c8bea3e339e630f5dff
DocErnstz <ernesto800@outlook.com>
2021-06-03 20:09:55 -0300

    client apollo graphql request

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,19 @@
 import React from "react";
+import { gql, useQuery } from "@apollo/client";
+
 
 const App: React.FC = () => {
+    const {data, loading} = useQuery(gql`
+    {
+        hello
+    }
+    `);
+    if (loading) {
+        return <div>loading...</div>
+    }
+    console.log(data);
     return (
-        <div>hello</div>
+        <div>{JSON.stringify(data)}</div>
     );
 }
 
--- web/src/index.tsx
+++ web/src/index.tsx
@@ -4,8 +4,9 @@ import ApolloClient from "apollo-boost";
 import {ApolloProvider} from "@apollo/react-hooks";
 import App from "./App";
 
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql"
+});
 
 
-ReactDOM.render(
-      <App/>,
-       document.getElementById("root"));
+ReactDOM.render(<ApolloProvider client={client as any }><App/></ApolloProvider>,document.getElementById("root"));
