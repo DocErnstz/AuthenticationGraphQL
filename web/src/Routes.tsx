7973d2779e47d0d47e861dd244c693441419b40d
DocErnstz <ernesto800@outlook.com>
2021-06-06 00:38:56 -0300

    basic routes

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -2,11 +2,15 @@ import React from "react";
 import { gql } from "@apollo/react-hooks"
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { useHelloQuery } from "./generated/graphql"; 
-
+import { Home } from "./pages/Home";
+import { Register } from "./pages/Register";
+import { Login } from "./pages/Login";
 const Routes: React.FC = () => {
     return (<BrowserRouter>
     <Switch>
-    <Route path="/" render={() => <div>hi!</div>}/>
+    <Route path="/" component={Home}/>
+    <Route path="/register" component={Register}/>
+    <Route path="/login" component={Login}/>    
     </Switch>
     </BrowserRouter>)
 }
